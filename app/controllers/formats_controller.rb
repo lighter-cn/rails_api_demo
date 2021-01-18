802c2355f862dcd78bafd5240599001868b8e285
lighter <zeke.kircheis@gmail.com>
2021-01-18 09:07:08 +0900

    Update formats_controller.rb

--- app/controllers/formats_controller.rb
+++ app/controllers/formats_controller.rb
@@ -24,10 +24,7 @@ class FormatsController < ApplicationController
       if formats == []
         raise StandardError,'error message'
       end
-      @formats = [];
-      formats.each do |format|
-        @formats << format.adjust
-      end
+      @formats = some_adjust(formats)
       render json: @formats
     rescue => e
       render_error(e)
@@ -38,6 +35,14 @@ class FormatsController < ApplicationController
   def render_error(e)
     @error = e.message
     render json: @error
+  end
+
+  def some_adjust(formats)
+    new_array = [];
+    formats.each do |format|
+      new_array << format.adjust
+    end
+    return new_array
   end
 
 end
